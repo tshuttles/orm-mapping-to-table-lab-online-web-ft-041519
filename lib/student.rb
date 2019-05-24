25f56fb0a27c68cc815be893c043a61a491d7142
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-24 01:11:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
   end 
   
   def self.create_table 
-    sql =  <<-SQL 
+    sql = <<-SQL 
       CREATE TABLE IF NOT EXISTS songs (
         id INTEGER PRIMARY KEY, 
         name TEXT, 
